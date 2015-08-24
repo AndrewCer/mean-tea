7e9d8b1d59d7d4b479380c0cd84b2fa4da77cccc
Andrew Cer <cer.andrew@gmail.com>
2015-08-24 17:00:27 -0600

    basic site structure complete

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var app = angular.module("appName", ['ngRoute']);
+
+
+app.config(function ($routeProvider, $locationProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'partials/home.html',
+      controller: 'HomeController'
+    })
+    // $locationProvider.html5Mode(true);
+    // .otherwise({redirectTo: '/'})
+})
